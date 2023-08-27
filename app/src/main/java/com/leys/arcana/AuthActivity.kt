d689835514bb5c921288b236571fc18f29649382
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2023-08-27 23:00:57 +0300

    Log in page design

--- app/src/main/java/com/leys/arcana/AuthActivity.kt
+++ app/src/main/java/com/leys/arcana/AuthActivity.kt
@@ -1,11 +1,78 @@
 package com.leys.arcana
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.leys.arcana.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityAuthBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
+        //if there is a current user already signed in then keep them signed in.
+        val currentUser = auth.currentUser
+
+        if(currentUser != null){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun LogIn(view : View){
+
+        val email = binding.EmailText.text.toString()
+        val password = binding.PasswordText.text.toString()
+
+        if(email == "" || password == ""){
+            Toast.makeText(this,"Please enter a valid email and password!", Toast.LENGTH_LONG).show()
+        }else{
+            auth.signInWithEmailAndPassword(email,password).addOnSuccessListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this,it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun ForgotPassword(view : View){
+
+    }
+
+    fun SignUp(view : View){
+
+        val email = binding.EmailText.text.toString()
+        val password = binding.PasswordText.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                //success
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            Toast.makeText(this,"Please enter a valid email and password!", Toast.LENGTH_LONG).show()
+        }
     }
 }
