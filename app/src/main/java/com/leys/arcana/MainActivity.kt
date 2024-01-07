43829a3e23398a33112551e91758f3cc3b2d46ae
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2024-01-08 01:14:34 +0300

    bug fixes

--- app/src/main/java/com/leys/arcana/MainActivity.kt
+++ app/src/main/java/com/leys/arcana/MainActivity.kt
@@ -2,13 +2,16 @@ package com.leys.arcana
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.leys.arcana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,29 +19,13 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> replaceFragment(HomePageFragment())
-                R.id.history -> replaceFragment(HistoryFragment())
-                R.id.profile -> replaceFragment(ProfileFragment())
-                R.id.settings -> replaceFragment(SettingsFragment())
+        setupWithNavController(bottomNavigationView, navController)
 
-                else->{
 
-                }
-            }
-
-            true
-        }
-
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout,fragment).commit()
 
     }
 
