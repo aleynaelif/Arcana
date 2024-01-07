1552b7bbf638a758284337a69a2b877c91cbd09a
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2024-01-07 23:38:31 +0300

    bottom navbar added

--- app/src/main/java/com/leys/arcana/MainActivity.kt
+++ app/src/main/java/com/leys/arcana/MainActivity.kt
@@ -2,6 +2,8 @@ package com.leys.arcana
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import com.leys.arcana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomePageFragment())
+                R.id.history -> replaceFragment(HistoryFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+                else->{
+
+                }
+            }
+
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment).commit()
 
     }
 
