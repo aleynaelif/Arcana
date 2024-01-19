3d7c1c574361d065a4070bad6024fa13b3d0d486
Aleyna Elif ÖZKAN <51717390+aleynaelif@users.noreply.github.com>
2024-01-20 02:00:05 +0300

    profile page updated

--- app/src/main/java/com/leys/arcana/ProfileFragment.kt
+++ app/src/main/java/com/leys/arcana/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.leys.arcana
 
+import android.content.res.Resources
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.leys.arcana.databinding.FragmentOneCardBinding
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.leys.arcana.databinding.FragmentProfileBinding
 
 
@@ -13,6 +17,8 @@ class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    var selectedBitmap : Bitmap? = null
+    private lateinit var database : SQLiteDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +30,28 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container,false)
-        return binding.root
+        val view = binding.root
+
+        // Find the Spinner in the layout
+        val spinnerGender: Spinner = binding.spinner
+
+        // Define an array of gender options
+        val genderOptions = resources.getStringArray(R.array.gender_options)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, genderOptions)
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+        spinnerGender.adapter = adapter
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 }
